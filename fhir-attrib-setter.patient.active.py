63f2cbeda6d09d3454b28d042d5e5d0c066be0c1
Justin McReynolds <mcjustin@uw.edu>
2024-09-13 13:22:30 -0700

    implement DRY_RUN config

--- fhir-attrib-setter.patient.active.py
+++ fhir-attrib-setter.patient.active.py
@@ -20,8 +20,12 @@ LOG_FILE = open(config['LOG_FILE_PATH'], "a", encoding="utf-8")
 fhir_endpoint = config['FHIR_ENDPOINT']
 fhir_auth_token = config['FHIR_AUTH_TOKEN']
 fume_endpoint = config['FUME_ENDPOINT']
+dry_run = config['DRY_RUN']
 
 log_it("=========================== STARTING RUN =============================")
+
+if dry_run > '0':
+    log_it('===THIS IS A DRY RUN, RESOURCES WILL NOT BE TOUCHED.===')
 
 # Set debug level, anything less than 9 is "info/warning", 9 or greater is "debug"
 debug_level = config['DEBUG_LEVEL']
@@ -67,23 +71,25 @@ if fhir_query_response is not None:
             log_it("Patient ID (" + str(pat_data['pat_id']) + ") matches criteria:" + activeStatusMsg)
             entry["resource"]["active"] = false
             
-            fhir_patient_response = session.put(fhir_endpoint + "/Patient/" + patient_hapi_id, json = entry["resource"], headers = fhir_patient_headers)
+            if dry_run < '1':
+            
+                fhir_patient_response = session.put(fhir_endpoint + "/Patient/" + patient_hapi_id, json = entry["resource"], headers = fhir_patient_headers)
 
-            if fhir_patient_response is not None:
-                fhir_patient_reply = fhir_patient_response.json()
-                if fhir_query_response.status_code != 201:
-                    log_it("ERROR: Unable to update/PUT Patient id " + patient_hapi_id + " at " + fhir_patient_response.url + ", skipping. Response code was " + fhir_query_response.status_code + "(!= 201) and json was: " + json.dumps(fhir_patient_reply))
-                else: 
-                    if debug_level > '8':
-                        log_it("FHIR patient PUT URL: " + fhir_patient_response.url)
-                    pat_cnt = pat_cnt + 1
-                    if debug_level > '8':
-                        log_it("FHIR patient PUT response: " + json.dumps(fhir_patient_reply))
-                    log_it("Successfully updated Patient " + patient_hapi_id)
-            else:
-                log_it("ERROR: Unable to update/PUT Patient id " + patient_hapi_id + "at " + fhir_patient_response.url + " (no response), skipping.")
-    else:
-        log_it("ERROR: Unable to query FHIR store for patients... exiting.")
+                if fhir_patient_response is not None:
+                    fhir_patient_reply = fhir_patient_response.json()
+                    if fhir_query_response.status_code != 201:
+                        log_it("ERROR: Unable to update/PUT Patient id " + patient_hapi_id + " at " + fhir_patient_response.url + ", skipping. Response code was " + fhir_query_response.status_code + "(!= 201) and json was: " + json.dumps(fhir_patient_reply))
+                    else: 
+                        if debug_level > '8':
+                            log_it("FHIR patient PUT URL: " + fhir_patient_response.url)
+                        pat_cnt = pat_cnt + 1
+                        if debug_level > '8':
+                            log_it("FHIR patient PUT response: " + json.dumps(fhir_patient_reply))
+                        log_it("Successfully updated Patient " + patient_hapi_id)
+                else:
+                    log_it("ERROR: Unable to update/PUT Patient id " + patient_hapi_id + "at " + fhir_patient_response.url + " (no response), skipping.")
+        else:
+            log_it("ERROR: Unable to query FHIR store for patients... exiting.")
 
 log_it("Total patients added/updated: " + str(pat_cnt))
 
