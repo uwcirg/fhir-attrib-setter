2ceec0589ff1ffc3e4a041f67651738e334236a0
Justin McReynolds <mcjustin@uw.edu>
2024-09-13 12:52:03 -0700

    logfile name change

--- fhir-attrib-setter.patient.active.ps1
+++ fhir-attrib-setter.patient.active.ps1
@@ -1,4 +1,4 @@
-﻿$Logfile = "O:\Workspaces\mcjustin\fhir-attrib-setter\prod\fhir-attrib-setter.patient.active.log"
+﻿$Logfile = "O:\Workspaces\mcjustin\fhir-attrib-setter.cosri.patient.active\prod\fhir-attrib-setter-cosri-patient-active.log"
 $Timestamp = Get-Date -UFormat "%Y-%m-%d %T"
 Add-content $Logfile -value "[$Timestamp] Running COSRI Patient active setter script..."
 cd "O:\Workspaces\mcjustin\fhir-attrib-setter\prod"
